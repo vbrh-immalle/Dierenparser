bbc57ba038091ea2a528d423c13aca8312838770
Meylemans Arne <arne.meylemans@immalle.eu>
2019-11-04 11:42:45 +0100

    Dataclass created

--- dierenparser.py
+++ dierenparser.py
@@ -1,27 +1,20 @@
+from dataclasses import dataclass
 
+
+@dataclass
 class Dier:
-
-    def __init__(self):
-        self.naam = ""
-        self.soort = ""
-        self.aantalPoten = 0
-        self.kleur = ""
-        self.geluid = ""
-
-    def print(self):
-        print(f'Het dier heet [{self.naam}], is van soort [{self.soort}], heeft [{self.aantalPoten}] poten, is [{self.kleur}] en maakt dit geluid: [{self.geluid}]!')    
+    naam: str
+    soort: ""
+    aantalPoten: 0
+    kleur: ""
+    geluid: ""
 
 
 def parse_line(line):
     naam, soort, aantalpoten, kleur, geluid = line.split(' - ')
-    d = Dier()
-    d.naam = naam
-    d.soort = soort
-    d.aantalPoten = int(aantalpoten)
-    d.kleur = kleur
-    d.geluid = geluid
-    print(f'Parsed: [{naam}] [{soort}] [{aantalpoten}] [{kleur}] [{geluid}]')
+    d = Dier(naam, soort, int(aantalpoten), kleur, geluid)
     return d
+
 
 def parse_text(str):
     dieren = []
@@ -35,6 +28,6 @@ if __name__ == '__main__':
     dieren = []
     with open('dieren.txt', 'r') as f:
         dieren = parse_text(f.read())
-    
+
     for dier in dieren:
-        dier.print()
+        print(dier)
